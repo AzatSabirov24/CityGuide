89cee4000307d426a2dcf04a1bd4c967e2e96275
Azat Sabirov <azatsabirov466@gmail.com>
2023-10-25 15:02:37 +0600

    create navigation drawer

---
+++ app/src/main/java/com/asabirov/cityguide/navigation/NavigationDrawerItem.kt
@@ -0,0 +1,9 @@
+package com.asabirov.cityguide.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
