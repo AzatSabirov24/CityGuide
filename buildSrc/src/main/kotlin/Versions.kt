0676751636abfb06aee2381c5e9b40c6710f27c9
Azat Sabirov <azatsabirov466@gmail.com>
2023-11-05 09:51:16 +0600

    upgrade deps versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    const val compose = "1.4.3"
-    const val composeMaterial3 = "1.1.1"
-    const val composeCompiler = "1.4.6"
+    const val compose = "1.5.4"
+    const val composeMaterial3 = "1.1.2"
+    const val composeCompiler = "1.5.3"
     const val hilt = "2.48"
     const val okHttp = "5.0.0-alpha.2"
     const val retrofit = "2.9.0"
