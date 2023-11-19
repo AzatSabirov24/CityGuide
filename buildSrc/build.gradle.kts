8a1e8e9ca9603a46a7f3c4d02be206a4efaddaad
Azat Sabirov <azatsabirov466@gmail.com>
2023-11-19 14:53:03 +0600

    update build:gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    implementation("com.android.tools.build:gradle:8.1.2")
+    implementation("com.android.tools.build:gradle:8.1.4")
     implementation("com.squareup:javapoet:1.13.0")
 }
 
