52841b402ceae5b55ed7fabc30407851618b33d3
Azat Sabirov <azatsabirov466@gmail.com>
2024-02-11 10:37:03 +0600

    v4

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -3,7 +3,7 @@ object ProjectConfig {
     const val minSdk = 24
     const val compileSdk = 34
     const val targetSdk = 34
-    const val versionCode = 3
-    const val versionName = "1.0.3"
+    const val versionCode = 4
+    const val versionName = "1.0.4"
     const val kotlinCompilerExtensionVersion = "1.5.3"
 }
